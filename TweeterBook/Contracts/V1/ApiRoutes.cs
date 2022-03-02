ac1c25928e8ed92ef6ec0995455a33980ffa8bf5
Max Phiri <Max.Phiri@isometrix.com>
2022-03-02 16:23:29 +0200

    Please enter the commit message for your changes. Lines starting

---
+++ TweeterBook/Contracts/V1/ApiRoutes.cs
@@ -0,0 +1,15 @@
+﻿namespace TweeterBook.Contracts.V1
+{
+    public static class ApiRoutes
+    {
+        public const string Root = "api";
+        public const string version = "v1";
+        public const string Base = Root + "/" + version;
+        public static class Posts
+        {
+            public const string GetAll = Base + " /posts";
+            public const string Create = Base + " /posts/" + "{id=postId,title=Maxwell}";
+            public const string Get = Base + " /posts/" + "{postId}";
+        }
+    }
+}
---
+++ TweeterBook/Controllers/V1/PostsController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using TweeterBook.Contracts.V1;
+using TweeterBook.Domain;
+
+namespace TweeterBook.Controllers.V1
+{
+    public class PostsController : Controller
+    {
+        private List<Post> _posts;
+
+        public PostsController()
+        {
+            _posts = new List<Post>();
+
+            for (int i = 0; i < 5; i++)
+            {
+                _posts.Add(new Post { Id = Guid.NewGuid().ToString()});
+            }
+        }
+
+        [HttpGet(ApiRoutes.Posts.GetAll)]
+        public IActionResult GetAll()
+        {
+            return Ok(_posts);
+        }
+    }
+}
